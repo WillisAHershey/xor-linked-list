f155019498a0775aa7c6891e953ecf27eb9fc29f
Willis Hershey <Willis.Hershey@druckerdiagnostics.com>
2022-03-09 11:52:47 -0500

    Parameterized list start and end

--- xorList.c
+++ xorList.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#ifndef LIST_START
+#define LIST_START 0
+#endif
+
+#ifndef LIST_END
+#define LIST_END 100
+#endif
+
+#if LIST_START >= LIST_END
+#error "LIST_START must be less than LIST_END"
+#endif
+
 struct xorList{
   int data;
   intptr_t nexxt; //This is a signed int type that is guaranteed to be wide enough to hold an entire pointer
@@ -32,13 +44,13 @@ int main(){
   struct xorList *tail;
   struct xorList *pt=head;
   intptr_t lastNode = (intptr_t)NULL;	//Technically NULL is not guaranteed to be (void*)0 but it is guaranteed to be unique so this is safe
-  for(int c=0;c<100;++c){
+  for(int c=LIST_START;c<LIST_END;++c){
 	struct xorList *newNode = malloc(sizeof(struct xorList));
 	*pt = (struct xorList){.data = c,.nexxt = (intptr_t)newNode ^ lastNode};
 	lastNode = (intptr_t)pt;
 	pt = newNode;
   }
-  *pt = (struct xorList){.data = 100,.nexxt = lastNode ^ (intptr_t)NULL};
+  *pt = (struct xorList){.data = LIST_END,.nexxt = lastNode ^ (intptr_t)NULL};
   tail = pt;
   printAll(head); //Despite this being two calls to the same function with opposite ends of the list, they will print all the data in the xor linked list in opposite directions
   printAll(tail);
